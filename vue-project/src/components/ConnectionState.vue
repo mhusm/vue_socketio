09b8d57879e6686274f7d50a4e5c994e576e7d28
Maria Husmann <maria.husmann@gmail.com>
2024-02-28 17:03:14 +0100

    initial commit

---
+++ vue-project/src/components/ConnectionState.vue
@@ -0,0 +1,38 @@
+
+<script setup>
+import { state, socket } from "@/socket";
+import { ref } from 'vue'
+
+const name = ref('')
+
+function sendEvent(event) {
+  socket.emit("name", name.value);
+}
+
+</script>
+<template>
+  <p>State: {{ connected }}</p>
+  <input type="text" v-model="name" />
+  <button @click="sendEvent">Send my name</button>
+  <p>The server says</p>
+  <p>{{ serverMessage }}</p>
+</template>
+  
+<script>
+import { state, socket } from "@/socket";
+
+export default {
+  name: "ConnectionState",
+
+  computed: {
+    connected() {
+      return state.connected;
+    },
+    serverMessage() {
+      return state.serverMessage;
+    }
+
+  }
+}
+
+</script>
